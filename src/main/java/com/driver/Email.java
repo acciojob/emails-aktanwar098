a06d0da50853e2f0785eb75ffd7806e9feac9aa2
aktanwar098 <ak.tanwar098@gmail.com>
2022-12-26 23:37:33 +0530

    first

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -10,6 +10,10 @@ public class Email {
         this.password = "Accio@123";
     }
 
+    public Email() {
+    }
+
+
     public String getEmailId() {
         return emailId;
     }
@@ -20,10 +24,62 @@ public class Email {
 
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+
+        if(this.password.equals(oldPassword)){
+            if(newPassword.length()>=8 && checkUpperCase(newPassword) && checkLowerCase(newPassword) && checkSpecialChar(newPassword)){
+
+                this.password = newPassword;
+            }
+        }
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
     }
+
+
+    //Function to check if any UPPER case letter is included in newPassword
+    public boolean checkUpperCase(String str)
+    {
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)>='A' && str.charAt(i)<='Z')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Function to check if any lower case letter is included in newpassword
+    public boolean checkLowerCase(String str)
+    {
+        for(int i=0; i<=str.length(); i++){
+            if(str.charAt(i) <= 'a' && str.charAt(i) >= 'z'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Function to check if any Special Character is included in newPassword
+    public boolean checkSpecialChar(String str){
+
+        String specialChar = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        for(int i=0; i<=str.length(); i++){
+            if(specialChar.contains(Character.toString(str.charAt(i)))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+
 }
+
+
